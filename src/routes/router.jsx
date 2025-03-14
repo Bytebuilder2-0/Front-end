6c1f88de948d93f96c7c9e1fe6ae237a5b2d25e0
Asry18 <asryamaz000@gmail.com>
2025-03-14 17:50:27 +0530

    Structure changes

---
+++ src/routes/router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from 'react-router-dom';
+import SupervisorDashboard from '../pages/supervisor/SupervisorDashboard';
+import SupInitial from '../pages/supervisor/SupInitial';
+import Inprogress from '../pages/supervisor/Inprogress';
+import TechnicianDashboard from '../pages/Technician/TechnicianDashboard';
+import CompletedSuper from '../pages/supervisor/CompletedSuper';
+import AppointmentSubmit from '../pages/User/AppoinmentSubmit';
+
+const router = createBrowserRouter([
+  {
+    path: "/Super",
+    element: <SupervisorDashboard />,
+  },
+  {
+    path: "/SInitial",
+    element: <SupInitial />,
+  },
+  {
+    path: "/SInpro",
+    element: <Inprogress />,
+  },
+  {
+    path: "/Scom",
+    element: <CompletedSuper />,
+  },
+  {
+    path: "/TDashboard",
+    element: <TechnicianDashboard />,
+  },
+  {
+    path: '/appointments/new',
+    element: <AppointmentSubmit />,
+  },
+]);
+
+export default router;
