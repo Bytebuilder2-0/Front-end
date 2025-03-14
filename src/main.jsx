a65794269416333cac3df88191dd190370d6ce4c
Asry18 <asryamaz000@gmail.com>
2025-03-14 17:50:27 +0530

    Structure changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import SupervisorDashboard from './pages/supervisor/SupervisorDashboard';
-import SupInitial from './pages/supervisor/SupInitial';
-import Inprogress from './pages/supervisor/Inprogress';
-import TechnicianDashboard from './pages/Technician/TechnicianDashboard';
-import CompletedSuper from './pages/supervisor/CompletedSuper';
-import AppointmentSubmit from './pages/User/AppoinmentSubmit';
-
-
-const router=createBrowserRouter(
-  [
-    {
-      path:"/Super",
-      element:<SupervisorDashboard/>
-    },{
-      path:"/SInitial",
-      element:<SupInitial/>
-    },{
-      path:"/SInpro",
-      element:<Inprogress/>
-    },{
-      path:"/Scom",
-      element:<CompletedSuper/>
-    },{
-      path:"/TDashboard",
-      element:<TechnicianDashboard/>
-    },
-    {
-      path: '/appointments/new', 
-      element: <AppointmentSubmit />,
-    }
-   
-  ]
-);
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/router'; // Importing the router 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
