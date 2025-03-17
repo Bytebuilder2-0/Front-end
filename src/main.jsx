263ea06e7efcb8d4a7cc3c68f6be89e5b5e7eadd
Asry18 <asryamaz000@gmail.com>
2025-03-17 14:37:47 +0530

    unique for initial page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import SupervisorDashboard from './pages/supervisor/SupervisorDashboard';
+
 import SupInitial from './pages/supervisor/SupInitial';
 
 const router=createBrowserRouter(
