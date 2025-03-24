eed417f9fd199184fa0ff8868e6b803a26ab9989
Asry18 <asryamaz000@gmail.com>
2025-03-24 15:11:45 +0530

    InvoiceView file setup with the icon named ReceiptIcon (An MUI icon)

--- src/components/sub/InvoiceView.jsx
+++ src/components/sub/InvoiceView.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ReceiptIcon from "@mui/icons-material/Receipt";
 
 function InvoiceView() {
   return (
-    <div>InvoiceView</div>
+   <ReceiptIcon
+                      fontSize="large"
+                      onClick={() => alert("Are you sure you want to delete this?")}
+                      style={{ cursor: "pointer" }}
+                    />
   )
 }
 
