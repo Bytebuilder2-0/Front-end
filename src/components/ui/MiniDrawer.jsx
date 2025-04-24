17c3f3941d57da1da40652f13899ac82ba4a2239
Asry18 <asryamaz000@gmail.com>
2025-04-24 23:06:59 +0530

    Notification count changed

--- src/components/ui/MiniDrawer.jsx
+++ src/components/ui/MiniDrawer.jsx
@@ -144,7 +144,7 @@ function MiniDrawer() {
               aria-label="show 17 new notifications"
               color="inherit"
             >
-              <Badge badgeContent={17} color="error">
+              <Badge badgeContent={0} color="error">
                 <NotificationsIcon sx={{ color: "text.primary" }}/>
               </Badge>
             </IconButton>
