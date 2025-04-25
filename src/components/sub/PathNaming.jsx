70de4868c844b77134d2768b56949161de315c66
Asry18 <asryamaz000@gmail.com>
2025-04-25 23:35:32 +0530

    Modified path with perfect alignments

--- src/components/sub/PathNaming.jsx
+++ src/components/sub/PathNaming.jsx
@@ -3,15 +3,19 @@ import { Typography, Box, Divider } from '@mui/material';
 
 function PathNaming({ label, path }) {
   return (
-    <Box >
+    <Box sx={{display:"flex",gap: 2}}>
        
       <img src="assets/path.png" alt="path image" />
+      <div>
       <Typography variant="h5" sx={{ fontWeight: 600 }}>
         {label}
       </Typography>
       <Typography variant="body2" color="text.secondary">
         {path}
       </Typography>
+
+      </div>
+    
    
     </Box>
   
