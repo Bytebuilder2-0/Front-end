0516e667b56cb44edf5bc0152c3fa917956f69fc
vijayanga <vijayanga123@gmail.com>
2025-04-25 17:26:02 +0530

    manager sidebar add feedback v1

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Typography, Paper } from "@mui/material";
+import FeedbackList from "./FeedbackList";
+
+const API_URL = "http://localhost:5000/api/feedback";
+
+const FeedbackPage = () => {
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  const fetchFeedbacks = async () => {
+    try {
+      const res = await axios.get(API_URL);
+      setFeedbacks(res.data.data);
+    } catch (error) {
+      console.error("Error fetching feedback:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFeedbacks();
+  }, []);
+
+  return (
+    <Container>
+      <Paper elevation={3} sx={{ padding: 3, marginTop: 2 }}>
+        <Typography variant="h5" gutterBottom>
+          User Feedback
+        </Typography>
+        <FeedbackList feedbacks={feedbacks} onUpdate={fetchFeedbacks} />
+      </Paper>
+    </Container>
+  );
+};
+
+export default FeedbackPage;
--- src/pages/Manager/FeedbackPage.jsx
+++ src/pages/Manager/FeedbackPage.jsx
@@ -1,38 +1,17 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Typography, Paper } from "@mui/material";
-import FeedbackList from "../../components/Feedback/FeedbackList";
+import Feedback from "../../components/Feedback/Feedback";
+import ManagerSidebar from "../../components/ui/ManagerMiniDrawer";
 
-const API_URL = "http://localhost:5000/api/feedback";
 
-const FeedbackPage = () => {
-  const [feedbacks, setFeedbacks] = useState([]);
 
-  const fetchFeedbacks = async () => {
-    try {
-      const res = await axios.get(API_URL);
-      setFeedbacks(res.data.data);
-    } catch (error) {
-      console.error("Error fetching feedback:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchFeedbacks();
-  }, []);
-
+export default function FeedbackPage() {
   return (
-    <ManagerSidebar>
-    <Container>
-      <Paper elevation={3} sx={{ padding: 3, marginTop: 2 }}>
-        <Typography variant="h5" gutterBottom>
-          User Feedback
-        </Typography>
-        <FeedbackList feedbacks={feedbacks} onUpdate={fetchFeedbacks} />
-      </Paper>
-    </Container>
-    </ManagerSidebar>
-  );
-};
-
-export default FeedbackPage;
+    <div>
+      <ManagerSidebar>
+      <Feedback/>
+      </ManagerSidebar>
+    </div>
+  )
+}
