7c04ab837dc7e06a50503d6412889e0b44dc0f2f
vijayanga <vijayanga123@gmail.com>
2025-04-25 14:33:56 +0530

    add side barto manager side

--- src/pages/Manager/ManageServices.jsx
+++ src/pages/Manager/ManageServices.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ServiceManager from "../../components/ServiceManage/ServiceManage";
+import ManagerSidebar from "../../components/ui/ManagerMiniDrawer";
 
 export default function ManageServices() {
   return (
     <div>
+      <ManagerSidebar>
       <ServiceManager />
+      </ManagerSidebar>
     </div>
   );
 }
