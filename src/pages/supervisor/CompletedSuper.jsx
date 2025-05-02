eaf7024bc47d12bcb9f9e6b2779fefe4fc37a12d
Asry18 <asryamaz000@gmail.com>
2025-05-02 09:48:10 +0530

    The formatting

--- src/pages/supervisor/CompletedSuper.jsx
+++ src/pages/supervisor/CompletedSuper.jsx
@@ -1,22 +1,22 @@
+import React from "react";
+import { Box, Toolbar, Divider } from "@mui/material";
+
 import CompletedS from "../../components/CompletedS";
-import { Box,Toolbar,Divider } from "@mui/material";
 import MiniDrawer from "../../components/ui/MiniDrawer";
 import PathNaming from "../../components/sub/PathNaming";
 
-function CompletedSuper(){
-return(
-    <Box sx={{ display: 'flex' }}>
-    <MiniDrawer />
-    <Box component="main" sx={{ flexGrow: 1, pl:3,pr:3  }}>
-     <Toolbar/>
-     <PathNaming label="Completed" path="Supervisor/Completed"/>
-       <Divider/>
-     <CompletedS/>
-    </Box>
-  </Box>
-  
-    
-);
+function CompletedSuper() {
+	return (
+		<Box sx={{ display: "flex" }}>
+			<MiniDrawer />
+			<Box component="main" sx={{ flexGrow: 1, pl: 3, pr: 3 }}>
+				<Toolbar />
+				<PathNaming label="Completed" path="Supervisor/Completed" />
+				<Divider />
+				<CompletedS />
+			</Box>
+		</Box>
+	);
 }
 
 export default CompletedSuper;
--- src/pages/supervisor/SupervisorDashboard.jsx
+++ src/pages/supervisor/SupervisorDashboard.jsx
@@ -1,22 +1,24 @@
+import React from "react";
+import { Box, Toolbar, Divider } from "@mui/material";
+
 import AppointmentData from "../../components/AppintmentData";
-import { Box,Toolbar,Divider } from "@mui/material";
 import MiniDrawer from "../../components/ui/MiniDrawer";
 import PathNaming from "../../components/sub/PathNaming";
 
 function SupervisorDashboard() {
-  return (
-     <>
-    <Box sx={{ display: 'flex' }}>
-       <MiniDrawer />
-       <Box component="main" sx={{ flexGrow: 1,pl:3,pr:3  }}>
-        <Toolbar/>
-         <PathNaming label="Dashboard" path="Supervisor/Dashboard"/>
-         <Divider/>
-    <AppointmentData/>
-       </Box>
-     </Box>
-     </>
-  );
+	return (
+		<>
+			<Box sx={{ display: "flex" }}>
+				<MiniDrawer />
+				<Box component="main" sx={{ flexGrow: 1, pl: 3, pr: 3 }}>
+					<Toolbar />
+					<PathNaming label="Dashboard" path="Supervisor/Dashboard" />
+					<Divider />
+					<AppointmentData />
+				</Box>
+			</Box>
+		</>
+	);
 }
 
 export default SupervisorDashboard;
