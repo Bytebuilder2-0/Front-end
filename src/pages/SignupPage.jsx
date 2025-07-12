b859f861e9f9b076a5f2011ea2bcfda69f988bfb
Asry18 <asryamaz000@gmail.com>
2025-07-12 23:30:15 +0530

    signup

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -76,7 +76,10 @@ const Signup = () => {
 				role: formData.role,
 			};
 
-			const res = await axios.post('${import.meta.env.VITE_API_BASE_URL}/auth/register', payload);
+			const res = await axios.post(
+				"${import.meta.env.VITE_API_BASE_URL}/auth/register",
+				payload
+			);
 			alert("Signup successful!");
 			console.log(res);
 			setFormData({
